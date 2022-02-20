5bdbdc75b0483d77651c0530d06588416e6a6838
Munauwar <munauwar.mughal@hva.nl>
2022-02-20 13:50:14 +0100

    get information from api

---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Coin = ({ image, name, symbol, price, volume, priceChange, marketcap}) => {
+  return (
+    <div className="flex justify-center">
+        <div className="coin-row">
+            <div className="coin">
+                <img src={image} alt="crypto" />
+                <h1>{name}</h1>
+                <p className="coin-symbol">{symbol}</p>
+            </div>
+            <div className="coin-data">
+                <p className="coin-price">€{price}</p>
+                <p className="coin-volume">€{volume.toLocaleString()}</p>
+                {priceChange < 0 ? (
+                    <p className="coin-percentage red">{priceChange.toFixed(2)}%</p>
+                ): (
+                    <p className="coin-percentage green">{priceChange.toFixed(2)}%</p>
+                )}    
+                <p className="coin-marketcap">
+                    Mkt cap: € {marketcap.toLocaleString()}
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Coin
